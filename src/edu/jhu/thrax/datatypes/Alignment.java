139296ac05465575bc0a88f83cfe2042c49d427a
Jonny <jweese@gmail.com>
2010-02-02 09:11:07 -0500

    added method to Alignment to check if two phrases are an initial phrase pair.

--- src/edu/jhu/thrax/datatypes/Alignment.java
+++ src/edu/jhu/thrax/datatypes/Alignment.java
@@ -1,5 +1,7 @@
 package edu.jhu.thrax.datatypes;
 
+import java.util.Arrays;
+
 public class Alignment {
 
 	private int [][] map;
@@ -7,5 +9,23 @@ public class Alignment {
 	public Alignment(int [][] m)
 	{
 		map = m;
+	}
+
+	public boolean isInitialPhrasePair(int x, int y, int a, int b)
+	{
+		boolean hasAlignment = false;
+		for (int i = 0; i < map.length; i++) {
+			int f = map[i][0];
+			int e = map[i][1];
+			if ((f >= x) && (f <= y)) {
+				if ((e < a) || (e > b)) {
+					return false;
+				}
+				else {
+					hasAlignment = true;
+				}
+			}
+		}
+		return hasAlignment;
 	}
 }
