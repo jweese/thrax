7324876c18850faef609cf60103a9fc618670fd0
Jonny Weese <jonny@cs.jhu.edu>
2010-07-29 10:17:56 -0400

    skeleton of lexprob calculator
    Forgot to include this in the last commit; RuleWritable now needs it in
    order to compile. (It needs the UNALIGNED constant for alignments.)

---
+++ src/edu/jhu/thrax/hadoop/features/LexicalProbability.java
@@ -0,0 +1,31 @@
+package edu.jhu.thrax.hadoop.features;
+
+import edu.jhu.thrax.ThraxConfig;
+import edu.jhu.thrax.hadoop.datatypes.IntPair;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+
+import java.io.IOException;
+
+public class LexicalProbability
+{
+    public static final Text UNALIGNED = new Text("/UNALIGNED/");
+
+    private static class Map extends Mapper<LongWritable, Text, IntPair, IntWritable>
+    {
+        public void map(LongWritable key, Text value, Context context)
+        {
+            String line = value.toString();
+            String [] parts = line.split(ThraxConfig.DELIMITER);
+        }
+    }
+
+}
+
