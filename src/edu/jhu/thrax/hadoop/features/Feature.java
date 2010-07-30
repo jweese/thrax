05351e049ec03758cea7704f3abf6a2450856daa
Jonny Weese <jonny@cs.jhu.edu>
2010-07-30 17:28:49 -0400

    improved hadoop Feature class definition

--- src/edu/jhu/thrax/hadoop/features/Feature.java
+++ src/edu/jhu/thrax/hadoop/features/Feature.java
@@ -1,13 +1,24 @@
 package edu.jhu.thrax.hadoop.features;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
 
 import edu.jhu.thrax.hadoop.datatypes.RuleWritable;
 
-public class Feature extends Mapper<RuleWritable, IntWritable,
+public abstract class Feature extends Mapper<RuleWritable, IntWritable,
                                     RuleWritable, IntWritable>
 {
-    // do nothing
+    public Class<? extends Reducer> combinerClass()
+    {
+        return IntSumReducer.class;
+    }
+
+    public abstract Class<? extends WritableComparator> sortComparatorClass();
+
+    public abstract Class<? extends Partitioner<RuleWritable, IntWritable>> partitionerClass();
 }
 
