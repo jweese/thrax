bf2e93d893f21aad299bb8ea244f45d59ed640df
Jonny Weese <jonny@cs.jhu.edu>
2010-08-01 17:45:51 -0400

    quick script to get a command line option

---
+++ lib/getoption.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl -w
+use strict;
+
+# this little script will read a Thrax config file and output a line that
+# has the given key, if it exists
+
+open CONF, $ARGV[0] or die "$!";
+my $key = $ARGV[1];
+while (<CONF>) {
+    s/#.*//;
+    print if /^$key\s+/;
+}
+
