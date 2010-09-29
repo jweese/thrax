02c0c744af3aebc9014b60f2876f30bd6c39c827
Jonny Weese <jonny@cs.jhu.edu>
2010-09-29 17:15:36 -0400

    ignore-NTs mode for Intersect
    If you pass Intersect "-X" as the first argument, it will ignore the
    actual nonterminal labels when calculating the intersection of two
    grammars, and only consider the shapes of the rules.

--- src/edu/jhu/thrax/util/Intersect.java
+++ src/edu/jhu/thrax/util/Intersect.java
@@ -3,6 +3,8 @@ package edu.jhu.thrax.util;
 import java.util.Scanner;
 import java.util.zip.GZIPInputStream;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.FileInputStream;
@@ -11,25 +13,42 @@ import java.io.File;
 
 public class Intersect
 {
-    private static HashMap<String,String> rules;
+    private static HashMap<String,ArrayList<String>> rules;
+    private static boolean ignoreNTs;
     public static void main(String [] argv) throws Exception
     {
-        getRulesFromFile(argv[0]);
+        String file1;
+        String file2;
+        String outputPrefix;
+        if (argv[0].equals("-X")) {
+            file1 = argv[1];
+            file2 = argv[2];
+            outputPrefix = argv[3];
+            ignoreNTs = true;
+        }
+        else {
+            file1 = argv[0];
+            file2 = argv[1];
+            outputPrefix = argv[2];
+            ignoreNTs = false;
+        }
+        getRulesFromFile(file1);
 
         Scanner scanner;
-        if (argv[1].endsWith(".gz"))
-            scanner = new Scanner(new GZIPInputStream(new FileInputStream(new File(argv[1]))), "UTF-8");
+        if (file2.endsWith(".gz"))
+            scanner = new Scanner(new GZIPInputStream(new FileInputStream(new File(file2))), "UTF-8");
         else
-            scanner = new Scanner(new File(argv[1]), "UTF-8");
-        PrintStream firstGrammar = new PrintStream(new FileOutputStream(argv[2] + ".1"));
-        PrintStream secondGrammar = new PrintStream(new FileOutputStream(argv[2] + ".2"));
+            scanner = new Scanner(new File(file2), "UTF-8");
+        PrintStream firstGrammar = new PrintStream(new FileOutputStream(outputPrefix + ".1"));
+        PrintStream secondGrammar = new PrintStream(new FileOutputStream(outputPrefix + ".2"));
         while (scanner.hasNextLine()) {
             String s = scanner.nextLine();
             String r = repr(s);
             if (rules.containsKey(r)) {
                 secondGrammar.println(s);
-                firstGrammar.println(rules.get(r));
-                rules.remove(r);
+                for (String x : rules.get(r))
+                    firstGrammar.println(x);
+                rules.get(r).clear();
             }
         }
         return;
@@ -37,12 +56,15 @@ public class Intersect
 
     private static String repr(String s)
     {
-        return s.substring(0, s.lastIndexOf("|||"));
+        String r = s.substring(0, s.lastIndexOf("|||"));
+        if (ignoreNTs) 
+            r = r.replaceAll("\\[[^]]+?\\]", "[X]");
+        return r;
     }
 
     private static void getRulesFromFile(String filename) throws IOException
     {
-        rules = new HashMap<String,String>();
+        rules = new HashMap<String,ArrayList<String>>();
         Scanner scanner;
         if (filename.endsWith(".gz")) {
             scanner = new Scanner(new GZIPInputStream(new FileInputStream(new File(filename))), "UTF-8");
@@ -52,7 +74,15 @@ public class Intersect
         }
         while (scanner.hasNextLine()) {
             String s = scanner.nextLine();
-            rules.put(repr(s), s);
+            String r = repr(s);
+            System.err.println(r);
+            if (rules.containsKey(r))
+                rules.get(r).add(s);
+            else {
+                ArrayList<String> al = new ArrayList<String>();
+                al.add(s);
+                rules.put(r, al);
+            }
         }
         return;
     }
