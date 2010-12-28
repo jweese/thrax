4bfc7914c69170b685fa1f37a2a698644eb57dd1
Jonny Weese <jonny@cs.jhu.edu>
2010-12-28 18:48:46 -0500

    remove SAMT feature set
    FeatureFactory updated to include each feature individually.

--- src/edu/jhu/thrax/hadoop/features/FeatureFactory.java
+++ src/edu/jhu/thrax/hadoop/features/FeatureFactory.java
@@ -36,8 +36,26 @@ public class FeatureFactory
         else if (name.equals("f2ephrase")) {
             return new TargetPhraseGivenSourceFeature();
         }
-        else if (name.equals("samt")) {
-            return new SAMTFeatureSet();
+        else if (name.equals("lexical")) {
+            return new LexicalityFeature();
+        }
+        else if (name.equals("abstract")) {
+            return new AbstractnessFeature();
+        }
+        else if (name.equals("adjacent")) {
+            return new AdjacentNonTerminalsFeature();
+        }
+        else if (name.equals("x-rule")) {
+            return new XRuleFeature();
+        }
+        else if (name.equals("source-terminals-without-target")) {
+            return new ConsumeSourceTerminalsFeature();
+        }
+        else if (name.equals("target-terminals-without-source")) {
+            return new ProduceTargetTerminalsFeature();
+        }
+        else if (name.equals("monotonic")) {
+            return new MonotonicFeature();
         }
         return null;
     }
