b9111687e86da21aa6b766e25612d2a1d9de2dfd
Jonny Weese <jonny@cs.jhu.edu>
2010-12-28 19:06:52 -0500

    fixed X feature to check for default NT in rule

--- src/edu/jhu/thrax/hadoop/features/XRuleFeature.java
+++ src/edu/jhu/thrax/hadoop/features/XRuleFeature.java
@@ -1,6 +1,7 @@
 package edu.jhu.thrax.hadoop.features;
 
 import edu.jhu.thrax.hadoop.datatypes.RuleWritable;
+import edu.jhu.thrax.ThraxConfig;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -10,6 +11,7 @@ public class XRuleFeature extends SimpleFeature
     private static final Text LABEL = new Text("ContainsX");
     private static final IntWritable ZERO = new IntWritable(0);
     private static final IntWritable ONE = new IntWritable(1);
+    private final String PATTERN = String.format("[%s", ThraxConfig.DEFAULT_NT);
 
     public XRuleFeature()
     {
@@ -18,7 +20,7 @@ public class XRuleFeature extends SimpleFeature
 
     public void score(RuleWritable r)
     {
-        r.features.put(LABEL, r.source.toString().matches("\\[X") ? ONE : ZERO);
+        r.features.put(LABEL, r.source.toString().indexOf(PATTERN) == -1 ? ZERO : ONE);
         return;
     }
 }
