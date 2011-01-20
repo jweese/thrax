aac7dabfbc4c7108af218919a275de8f3431c73a
Jonny Weese <jonny@cs.jhu.edu>
2011-01-20 15:44:08 -0500

    initial job base class and state

---
+++ src/edu/jhu/thrax/hadoop/jobs/ThraxJob.java
@@ -0,0 +1,23 @@
+package edu.jhu.thrax.hadoop.jobs;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.conf.Configuration;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import java.io.IOException;
+
+public abstract class ThraxJob
+{
+    public static Job getJob(Configuration conf) throws IOException
+    {
+        return new Job(conf);
+    }
+
+    public static Set<Class<? extends Job>> getPrerequisites()
+    {
+        return new HashSet<Class<? extends Job>>();
+    }
+}
+
