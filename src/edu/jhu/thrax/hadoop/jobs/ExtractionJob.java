8976111d4eebd11bfe3568c162b868fec25e1db3
Matt Post <post@jhu.edu>
2011-02-03 17:58:20 -0500

    added maximum split size config file param

--- src/edu/jhu/thrax/hadoop/jobs/ExtractionJob.java
+++ src/edu/jhu/thrax/hadoop/jobs/ExtractionJob.java
@@ -33,6 +33,11 @@ public class ExtractionJob extends ThraxJob
         job.setOutputFormatClass(SequenceFileOutputFormat.class);
 
         FileInputFormat.setInputPaths(job, new Path(conf.get("thrax.input-file")));
+        int maxSplitSize = conf.getInt("thrax.max-split-size", 0);
+        if (maxSplitSize != 0) {
+//            System.err.println("* changing max split size from " + FileInputFormat.getMaxSplitSize(job) + " to " + maxSplitSize);
+            FileInputFormat.setMaxInputSplitSize(job, maxSplitSize);
+        }
         FileOutputFormat.setOutputPath(job, new Path(conf.get("thrax.work-dir") + "rules"));
 
         return job;
