c1aed5e50b1328162b867c708fcb6fda0e256a28
Jonny Weese <jonny@cs.jhu.edu>
2011-02-07 18:51:20 -0500

    shell script wrapper for test set filter

---
+++ scripts/filter_rules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if (($# < 2))
+then
+    echo "usage: filter_rules.sh <phrase length> <test set>"
+    exit 1
+fi
+
+java -cp $THRAX/bin/thrax.jar edu.jhu.thrax.util.TestSetFilter $*
+
