93aa2b6259be917b20bc2cc90b6260092c0d59ec
Jonny Weese <jonny@cs.jhu.edu>
2011-03-02 10:26:12 -0500

    malformed input exception subclasses

---
+++ src/edu/jhu/thrax/util/exceptions/EmptySourceSentenceException.java
@@ -0,0 +1,7 @@
+package edu.jhu.thrax.util.exceptions;
+
+public class EmptySourceSentenceException extends MalformedInputException
+{
+    private static final long serialVersionUID = 2132L;
+}
+
---
+++ src/edu/jhu/thrax/util/exceptions/InconsistentAlignmentException.java
@@ -0,0 +1,7 @@
+package edu.jhu.thrax.util.exceptions;
+
+public class InconsistentAlignmentException extends MalformedInputException
+{
+    private static final long serialVersionUID = 33L;
+}
+
