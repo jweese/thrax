b0d0c01aa26829f9d8f984aed17b05a9936637a2
Jonny Weese <jonny@cs.jhu.edu>
2011-03-03 21:52:14 -0500

    removed ThraxConfig ref from extractor factory

--- src/edu/jhu/thrax/extraction/RuleExtractorFactory.java
+++ src/edu/jhu/thrax/extraction/RuleExtractorFactory.java
@@ -1,6 +1,5 @@
 package edu.jhu.thrax.extraction;
 
-import edu.jhu.thrax.ThraxConfig;
 import edu.jhu.thrax.util.exceptions.ConfigurationException;
 
 import org.apache.hadoop.conf.Configuration;
@@ -23,11 +22,13 @@ public class RuleExtractorFactory {
     public static RuleExtractor create(Configuration conf) throws ConfigurationException
     {
         String gt = conf.get("thrax.grammar", "NONE");
+        boolean SOURCE_IS_PARSED = conf.getBoolean("thrax.source-is-parsed", false);
+        boolean TARGET_IS_PARSED = conf.getBoolean("thrax.target-is-parsed", false);
         if (gt.equals(HieroRuleExtractor.name)) {
             return new HieroRuleExtractor(conf);
         }
         else if (gt.equals(SAMTExtractor.name)) {
-            if (!(ThraxConfig.SOURCE_IS_PARSED || ThraxConfig.TARGET_IS_PARSED))
+            if (!(SOURCE_IS_PARSED || TARGET_IS_PARSED))
                 throw new ConfigurationException("SAMT requires that either the source or target sentences be parsed");
             return new SAMTExtractor(conf);
         }
