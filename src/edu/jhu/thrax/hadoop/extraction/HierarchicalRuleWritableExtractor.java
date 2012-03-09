80d769aa44c553f037095d8663e9a398fa07c2a7
Jonny Weese <jonny@cs.jhu.edu>
2012-03-09 14:35:16 -0500

    hadoop Hierarchical extractor: handle input

--- src/edu/jhu/thrax/hadoop/extraction/HierarchicalRuleWritableExtractor.java
+++ src/edu/jhu/thrax/hadoop/extraction/HierarchicalRuleWritableExtractor.java
@@ -4,11 +4,20 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import edu.jhu.thrax.util.io.InputUtilities;
+import edu.jhu.thrax.util.exceptions.MalformedInputException;
 import edu.jhu.thrax.hadoop.datatypes.RuleWritable;
+import edu.jhu.thrax.datatypes.*;
+
+import java.util.Collections;
 
 public class HierarchicalRuleWritableExtractor implements RuleWritableExtractor
 {
 	private Mapper.Context context;
+
+	private boolean sourceParsed;
+	private boolean targetParsed;
+	private boolean reverse;
 
 	public HierarchicalRuleWritableExtractor(Mapper.Context c)
 	{
@@ -17,6 +26,14 @@ public class HierarchicalRuleWritableExtractor implements RuleWritableExtractor
 
 	public Iterable<RuleWritable> extract(Text line)
 	{
+		AlignedSentencePair sentencePair;
+		try {
+			sentencePair = InputUtilities.alignedSentencePair(line.toString(), sourceParsed, targetParsed, reverse);
+		}
+		catch (MalformedInputException e) {
+			context.getCounter("input errors", e.getMessage()).increment(1);
+			return Collections.<RuleWritable>emptyList();
+		}
 		return null;
 	}
 }
