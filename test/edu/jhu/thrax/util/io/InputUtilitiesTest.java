4f9f487a6ab0d8bcf9edc2c5f9725d176732634f
Jonny Weese <jonny@cs.jhu.edu>
2012-03-09 14:18:16 -0500

    InputUtilitiesTest: fix exceptions

--- test/edu/jhu/thrax/util/io/InputUtilitiesTest.java
+++ test/edu/jhu/thrax/util/io/InputUtilitiesTest.java
@@ -7,30 +7,30 @@ import edu.jhu.thrax.util.exceptions.*;
 public class InputUtilitiesTest
 {
     @Test
-    public void parseYield_EmptyString_ReturnsZeroLengthArray() throws MalformedParseException
+    public void parseYield_EmptyString_ReturnsZeroLengthArray() throws MalformedInputException
     {
         Assert.assertEquals(InputUtilities.parseYield("").length, 0);
     }
 
     @Test
-    public void parseYield_Whitespace_ReturnsZeroLengthArray() throws MalformedParseException
+    public void parseYield_Whitespace_ReturnsZeroLengthArray() throws MalformedInputException
     {
         Assert.assertEquals(InputUtilities.parseYield("        ").length, 0);
     }
     @Test
-    public void parseYield_EmptyParse_ReturnsZeroLengthArray() throws MalformedParseException
+    public void parseYield_EmptyParse_ReturnsZeroLengthArray() throws MalformedInputException
     {
         Assert.assertEquals(InputUtilities.parseYield("()").length, 0);
     }
 
-    @Test(expectedExceptions = { MalformedParseException.class })
-    public void parseYield_UnbalancedLeft_ThrowsException() throws MalformedParseException
+    @Test(expectedExceptions = { MalformedInputException.class })
+    public void parseYield_UnbalancedLeft_ThrowsException() throws MalformedInputException
     {
         InputUtilities.parseYield("(S (DT the) (NP dog)");
     }
 
-    @Test(expectedExceptions = { MalformedParseException.class })
-    public void parseYield_UnbalancedRight_ThrowsException() throws MalformedParseException
+    @Test(expectedExceptions = { MalformedInputException.class })
+    public void parseYield_UnbalancedRight_ThrowsException() throws MalformedInputException
     {
         InputUtilities.parseYield("(S (DT the) (NP dog)))");
     }
@@ -49,9 +49,12 @@ public class InputUtilitiesTest
         Assert.assertEquals(InputUtilities.getWords("    ", true).length, 0);
     }
 
+	@Test
     public void getWords_PlainWords_ReturnsStringArray() throws MalformedInputException
     {
         String [] tokens = { "hello", ",", "world" };
         Assert.assertEquals(InputUtilities.getWords("hello , world", false), tokens);
     }
+
 }
+
