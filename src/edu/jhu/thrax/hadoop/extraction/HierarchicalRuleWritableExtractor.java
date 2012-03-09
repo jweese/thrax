469882c519272eca6b8457f7bcb5f2784e22e32f
Jonny Weese <jonny@cs.jhu.edu>
2012-03-09 14:54:06 -0500

    hierarchical RuleWritable: do extraction
    Still needs to initialize all the parameters, and stuff. Also needs to
    implement the toRuleWritable method.

--- src/edu/jhu/thrax/hadoop/extraction/HierarchicalRuleWritableExtractor.java
+++ src/edu/jhu/thrax/hadoop/extraction/HierarchicalRuleWritableExtractor.java
@@ -8,8 +8,11 @@ import edu.jhu.thrax.util.io.InputUtilities;
 import edu.jhu.thrax.util.exceptions.MalformedInputException;
 import edu.jhu.thrax.hadoop.datatypes.RuleWritable;
 import edu.jhu.thrax.datatypes.*;
+import edu.jhu.thrax.extraction.*;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 public class HierarchicalRuleWritableExtractor implements RuleWritableExtractor
 {
@@ -18,6 +21,9 @@ public class HierarchicalRuleWritableExtractor implements RuleWritableExtractor
 	private boolean sourceParsed;
 	private boolean targetParsed;
 	private boolean reverse;
+
+	private HierarchicalRuleExtractor extractor;
+	private SpanLabeler labeler;
 
 	public HierarchicalRuleWritableExtractor(Mapper.Context c)
 	{
@@ -34,6 +40,18 @@ public class HierarchicalRuleWritableExtractor implements RuleWritableExtractor
 			context.getCounter("input errors", e.getMessage()).increment(1);
 			return Collections.<RuleWritable>emptyList();
 		}
+		String [] source = sentencePair.source;
+		String [] target = sentencePair.target;
+		Alignment alignment = sentencePair.alignment;
+		List<HierarchicalRule> rules = extractor.extract(source.length, target.length, alignment);
+		List<RuleWritable> result = new ArrayList<RuleWritable>(rules.size());
+		for (HierarchicalRule r : rules)
+			result.add(toRuleWritable(r, labeler, source, target));
+		return result;
+	}
+
+	private static RuleWritable toRuleWritable(HierarchicalRule r, SpanLabeler spanLabeler, String [] source, String [] target)
+	{
 		return null;
 	}
 }
