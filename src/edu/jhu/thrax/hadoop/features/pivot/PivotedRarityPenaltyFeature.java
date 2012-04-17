6610c201d91aaa610db5deaf5c9a9145d9ac8db8
Juri Ganitkevitch <j.ganitkevitch@gmail.com>
2012-04-17 15:30:00 -0400

    Fixed rarity penalty aggregation from min to sum over underlying counts. Reformatted.

--- src/edu/jhu/thrax/hadoop/features/pivot/PivotedRarityPenaltyFeature.java
+++ src/edu/jhu/thrax/hadoop/features/pivot/PivotedRarityPenaltyFeature.java
@@ -11,63 +11,71 @@ import org.apache.hadoop.io.Writable;
 
 public class PivotedRarityPenaltyFeature implements PivotedFeature {
 
-	private static final Text LABEL = new Text("RarityPenalty");
+  private static final Text LABEL = new Text("RarityPenalty");
 
-	private static final DoubleWritable ZERO = new DoubleWritable(0.0);
-	
-	private double min_rp;
+  private static final DoubleWritable ZERO = new DoubleWritable(0.0);
+  
+  private static final double RENORMALIZE = Math.exp(-1);
 
-	public String getName() {
-		return "rarity";
-	}
+  private double aggregated_rp;
 
-	public Text getFeatureLabel() {
-		return LABEL;
-	}
+  public String getName() {
+    return "rarity";
+  }
 
-	public Set<String> getPrerequisites() {
-		Set<String> prereqs = new HashSet<String>();
-		prereqs.add("rarity");
-		return prereqs;
-	}
+  public Text getFeatureLabel() {
+    return LABEL;
+  }
 
-	public DoubleWritable pivot(MapWritable a, MapWritable b) {
-		double a_rp = ((DoubleWritable) a.get(new Text("RarityPenalty"))).get();
-		double b_rp = ((DoubleWritable) b.get(new Text("RarityPenalty"))).get();
-		return new DoubleWritable(Math.max(a_rp, b_rp));
-	}
+  public Set<String> getPrerequisites() {
+    Set<String> prereqs = new HashSet<String>();
+    prereqs.add("rarity");
+    return prereqs;
+  }
 
-	public void unaryGlueRuleScore(Text nt, Map<Text, Writable> map) {
-		map.put(LABEL, ZERO);
-	}
+  public DoubleWritable pivot(MapWritable a, MapWritable b) {
+    double a_rp = ((DoubleWritable) a.get(new Text("RarityPenalty"))).get();
+    double b_rp = ((DoubleWritable) b.get(new Text("RarityPenalty"))).get();
+    return new DoubleWritable(Math.max(a_rp, b_rp));
+  }
 
-	public void binaryGlueRuleScore(Text nt, Map<Text, Writable> map) {
-		map.put(LABEL, ZERO);
-	}
+  public void unaryGlueRuleScore(Text nt, Map<Text, Writable> map) {
+    map.put(LABEL, ZERO);
+  }
 
-	public void initializeAggregation() {
-		min_rp = Double.MAX_VALUE;
-	}
+  public void binaryGlueRuleScore(Text nt, Map<Text, Writable> map) {
+    map.put(LABEL, ZERO);
+  }
 
-	public void aggregate(MapWritable a) {
-		double rp = ((DoubleWritable) a.get(LABEL)).get();
-		if (rp < min_rp)
-			min_rp = rp;
-	}
+  public void initializeAggregation() {
+    aggregated_rp = -1;
+  }
 
-	public DoubleWritable finalizeAggregation() {
-		return new DoubleWritable(min_rp);
-	}
+  public void aggregate(MapWritable a) {
+    double rp = ((DoubleWritable) a.get(LABEL)).get();
+    if (aggregated_rp == -1) {
+      aggregated_rp = rp;
+    } else {
+      // Rarity is exp(1 - count). To compute rarity over a sum of counts:
+      // rarity_{1+2} = exp(1 - (count_1 + count_2)) = exp(1 - count_1) * exp(-count_2) = 
+      //     = exp(1 - count_1) * exp(1 - count_2) * exp(-1) = rarity_1 * rarity_2 * exp(-1) 
+      aggregated_rp *= rp * RENORMALIZE;
+    }
+  }
 
-	@Override
-	public Set<Text> getLowerBoundLabels() {
-		Set<Text> lower_bound_labels = new HashSet<Text>();
-		lower_bound_labels.add(new Text("RarityPenalty"));
-		return lower_bound_labels;
-	}
+  public DoubleWritable finalizeAggregation() {
+    return new DoubleWritable(aggregated_rp);
+  }
 
-	@Override
-	public Set<Text> getUpperBoundLabels() {
-		return null;
-	}
+  @Override
+  public Set<Text> getLowerBoundLabels() {
+    Set<Text> lower_bound_labels = new HashSet<Text>();
+    lower_bound_labels.add(new Text("RarityPenalty"));
+    return lower_bound_labels;
+  }
+
+  @Override
+  public Set<Text> getUpperBoundLabels() {
+    return null;
+  }
 }
