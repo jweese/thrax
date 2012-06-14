7f2b74fa076e8679cac4ac2306c0642dc6e421db
Matt Post <post@cs.jhu.edu>
2012-06-14 09:58:18 -0400

    updated script to use Hadoop version

--- scripts/create_glue_grammar.sh
+++ scripts/create_glue_grammar.sh
@@ -18,5 +18,5 @@ then
     exit 1
 fi
 
-java -cp $THRAX/bin/thrax.jar:$HADOOP/hadoop-0.20.2-core.jar:$HADOOP/lib/commons-logging-1.0.4.jar edu.jhu.thrax.util.CreateGlueGrammar $1
+java -cp $THRAX/bin/thrax.jar:$HADOOP/hadoop-core-$HADOOP_VERSION.jar:$HADOOP/lib/commons-logging-1.1.1.jar edu.jhu.thrax.util.CreateGlueGrammar $1
 
