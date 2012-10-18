41b26d552d2fc8f35857fa1b891962a11aad71b1
Matt Post <post@cs.jhu.edu>
2012-10-18 11:08:40 -0400

    updated to new glue file format

--- src/edu/jhu/thrax/util/CreateGlueGrammar.java
+++ src/edu/jhu/thrax/util/CreateGlueGrammar.java
@@ -22,8 +22,12 @@ public class CreateGlueGrammar
     private static TreeMap<Text,Writable> unaryFeatures;
     private static TreeMap<Text,Writable> binaryFeatures;
 
-    private static final String RULE_ONE = "[%1$s] ||| [%2$s,1] ||| [%2$s,1] ||| ";
-    private static final String RULE_TWO = "[%1$s] ||| [%1$s,1] [%2$s,2] ||| [%1$s,1] [%2$s,2] ||| ";
+    // [GOAL] ||| <s> ||| <s> ||| 0
+    // [GOAL] ||| [GOAL,1] [X,2] ||| [GOAL,1] [X,2] ||| -1
+    // [GOAL] ||| [GOAL,1] </s> ||| [GOAL,1] </s> ||| 0
+    private static final String RULE_START = "[%1$s] ||| <s> ||| <s> ||| 0";
+    private static final String RULE_TWO = "[%1$s] ||| [%1$s,1] [%2$s,2] ||| [%1$s,1] [%2$s,2] ||| -1";
+    private static final String RULE_END = "[%1$s] ||| [%1$s,1] </s> ||| [%1$s,1] </s> ||| 0";
     private static String GOAL = "GOAL";
     private static boolean LABEL = false;
     private static String [] FEATURES;
@@ -35,18 +39,9 @@ public class CreateGlueGrammar
             return;
         }
 
-        unaryFeatures = new TreeMap<Text,Writable>();
-        binaryFeatures = new TreeMap<Text,Writable>();
-
         Map<String,String> opts = ConfFileParser.parse(argv[0]);
         if (opts.containsKey("goal-symbol"))
             GOAL = opts.get("goal-symbol");
-        if (opts.containsKey("label-feature-scores"))
-            LABEL = Boolean.parseBoolean(opts.get("label-feature-scores"));
-        if (opts.containsKey("features"))
-            FEATURES = opts.get("features").split("\\s+");
-        else
-            FEATURES = new String[0];
 
         Scanner scanner = new Scanner(System.in, "UTF-8");
         nts = new HashSet<String>();
@@ -61,43 +56,12 @@ public class CreateGlueGrammar
             String lhs = line.substring(lhsStart, lhsEnd);
             nts.add(lhs);
         }
+        System.out.println(String.format(RULE_START, GOAL));
         for (String nt : nts) {
             Text n = new Text(nt);
-            unaryFeatures.clear();
-            binaryFeatures.clear();
-            for (String f : FEATURES) {
-                SimpleFeature sf = SimpleFeatureFactory.get(f);
-                if (sf != null) {
-                    sf.unaryGlueRuleScore(n, unaryFeatures);
-                    sf.binaryGlueRuleScore(n, binaryFeatures);
-                    continue;
-                }
-                MapReduceFeature mrf = FeatureJobFactory.get(f);
-                if (mrf != null) {
-                    mrf.unaryGlueRuleScore(n, unaryFeatures);
-                    mrf.binaryGlueRuleScore(n, binaryFeatures);
-                }
-            }
-            StringBuilder r1 = new StringBuilder();
-            r1.append(String.format(RULE_ONE, GOAL, n));
-            StringBuilder r2 = new StringBuilder();
-            r2.append(String.format(RULE_TWO, GOAL, n));
-
-            for (Text t : unaryFeatures.keySet()) {
-                if (LABEL) {
-                    r1.append(String.format("%s=%s ", t, unaryFeatures.get(t)));
-                    r2.append(String.format("%s=%s ", t, binaryFeatures.get(t)));
-                }
-                else {
-                    r1.append(String.format("%s ", unaryFeatures.get(t)));
-                    r2.append(String.format("%s ", binaryFeatures.get(t)));
-                }
-            }
-
-            System.out.println(r1.toString());
-            System.out.println(r2.toString());
+            System.out.println(String.format(RULE_TWO, GOAL, n));
         }
+        System.out.println(String.format(RULE_END, GOAL));
     }
 
 }
-
