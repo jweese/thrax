c33768ea4f6547577e0958e89839b732a5b7587d
Juri Ganitkevitch <j.ganitkevitch@gmail.com>
2013-04-18 12:48:43 -0400

    CreateGlueGrammar works with packed grammars now.

--- src/edu/jhu/thrax/util/CreateGlueGrammar.java
+++ src/edu/jhu/thrax/util/CreateGlueGrammar.java
@@ -1,9 +1,11 @@
 package edu.jhu.thrax.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Scanner;
+
+import edu.jhu.thrax.util.io.LineReader;
 
 public class CreateGlueGrammar {
   private static HashSet<String> nts;
@@ -11,40 +13,61 @@ public class CreateGlueGrammar {
   // [GOAL] ||| [GOAL,1] [X,2] ||| [GOAL,1] [X,2] ||| -1
   // [GOAL] ||| [GOAL,1] </s> ||| [GOAL,1] </s> ||| 0
   // [GOAL] ||| <s> ||| <s> ||| 0
-  private static final String RULE_START = "[%1$s] ||| <s> ||| <s> ||| 0";
-  private static final String RULE_TWO =
-      "[%1$s] ||| [%1$s,1] [%2$s,2] ||| [%1$s,1] [%2$s,2] ||| -1";
-  private static final String RULE_END = "[%1$s] ||| [%1$s,1] </s> ||| [%1$s,1] </s> ||| 0";
+
+  private static final String R_START = "[%1$s] ||| <s> ||| <s> ||| 0";
+  private static final String R_TWO = "[%1$s] ||| [%1$s,1] [%2$s,2] ||| [%1$s,1] [%2$s,2] ||| -1";
+  private static final String R_END = "[%1$s] ||| [%1$s,1] </s> ||| [%1$s,1] </s> ||| 0";
 
   // [GOAL] ||| <s> [X,1] </s> ||| <s> [X,1] </s> ||| 0
-  private static final String RULE_TOP = "[%1$s] ||| <s> [%2$s,1] </s> ||| <s> [%2$s,1] </s> ||| 0";
+  private static final String R_TOP = "[%1$s] ||| <s> [%2$s,1] </s> ||| <s> [%2$s,1] </s> ||| 0";
 
   private static String GOAL = "GOAL";
 
   public static void main(String[] argv) throws IOException {
+    String grammar_file_name = null;
     if (argv.length > 0) {
       Map<String, String> opts = ConfFileParser.parse(argv[0]);
       if (opts.containsKey("goal-symbol")) GOAL = opts.get("goal-symbol");
+      if (opts.containsKey("grammar")) GOAL = opts.get("grammar");
+    }
+    if (grammar_file_name == null) {
+      System.err.println("No grammar specified.");
+      System.exit(1);
+    }
+    File grammar_file = new File(grammar_file_name);
+    if (!grammar_file.exists()) {
+      System.err.println("Grammar file doesn't exist: " + grammar_file_name);
+      System.exit(1);
     }
 
-    Scanner scanner = new Scanner(System.in, "UTF-8");
     nts = new HashSet<String>();
-    while (scanner.hasNextLine()) {
-      String line = scanner.nextLine();
-      int lhsStart = line.indexOf("[") + 1;
-      int lhsEnd = line.indexOf("]");
-      if (lhsStart < 1 || lhsEnd < 0) {
-        System.err.printf("malformed rule: %s\n", line);
-        continue;
+    if (grammar_file.isDirectory()) {
+      Vocabulary.read(grammar_file_name + File.separator + "vocabulary");
+      for (int i = 0; i < Vocabulary.size(); ++i) {
+        String token = Vocabulary.word(i);
+        if (Vocabulary.nt(token)) nts.add(token.substring(1, token.length()));
       }
-      String lhs = line.substring(lhsStart, lhsEnd);
-      nts.add(lhs);
+    } else {
+      LineReader reader = new LineReader(grammar_file_name);
+      while (reader.hasNext()) {
+        String line = reader.next();
+        int lhsStart = line.indexOf("[") + 1;
+        int lhsEnd = line.indexOf("]");
+        if (lhsStart < 1 || lhsEnd < 0) {
+          System.err.printf("malformed rule: %s\n", line);
+          continue;
+        }
+        String lhs = line.substring(lhsStart, lhsEnd);
+        nts.add(lhs);
+      }
     }
-    System.out.println(String.format(RULE_START, GOAL));
+
+    System.out.println(String.format(R_START, GOAL));
     for (String nt : nts)
-      System.out.println(String.format(RULE_TWO, GOAL, nt));
-    System.out.println(String.format(RULE_END, GOAL));
+      System.out.println(String.format(R_TWO, GOAL, nt));
+    System.out.println(String.format(R_END, GOAL));
     for (String nt : nts)
-      System.out.println(String.format(RULE_TOP, GOAL, nt));
+      System.out.println(String.format(R_TOP, GOAL, nt));
+
   }
 }
